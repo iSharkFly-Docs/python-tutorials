27e4ac217a08f01aca92ebbfa3b919334f850bec
YuCheng Hu <yuchenghu@ossez.com>
2021-03-05 16:20:33 -0500

    重新格式化所有的代码，并且导入需要的内容

---
+++ sample/LoopClauses.py
@@ -0,0 +1,23 @@
+# for n in range(2, 10):
+#     for x in range(2, n):
+#         if n % x == 0:
+#             print(n, 'equals', x, '*', n // x)
+#             break
+#     else:
+#         # loop fell through without finding a factor
+#         print(n, 'is a prime number')
+#
+# #
+# for num in range(2, 10):
+#     if num % 2 == 0:
+#         print("Found an even number", num)
+#         continue
+#     print("Found a number", num)
+
+# alphabet = ["A", "B", "C"]
+# for x in alphabet:
+#     print(x)
+
+strOssez = "ossez.com"
+for x in strOssez:
+    print(x)
