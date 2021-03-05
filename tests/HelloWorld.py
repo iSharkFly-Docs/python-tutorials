003ea2f9d47cd6407fbb4d95153f32926e401a39
YuCheng Hu <yuchenghu@ossez.com>
2021-03-05 15:34:06 -0500

    将测试的文件进行提交

---
+++ tests/HelloWorld.py
@@ -0,0 +1,9 @@
+# 第一个 Python 程序 - Hello, World!
+# Version: 0.1
+# Author: YuCheng Hu
+
+
+print('hello, world!')
+print('你好', '世界')
+print('hello', 'world', sep=', ', end='!')
+print('goodbye, world', end='!\n')
